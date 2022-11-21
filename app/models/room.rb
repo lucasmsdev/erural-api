f0df71c6b169d8ad961ed0509e1c91c840e46a29
LucasMachado9 <lucasdevoficial@gmail.com>
2022-11-20 23:10:49 -0300

    final commit

---
+++ app/models/room.rb
@@ -0,0 +1,8 @@
+class Room < ApplicationRecord
+  has_many :comments
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :youtube_url, presence: true
+end
---
+++ app/views/rooms/index.json.jbuilder
@@ -0,0 +1,5 @@
+json.array!   @rooms do |room|
+json.id room.id
+json.name  room.name
+json.image_url room.image_url
+end
